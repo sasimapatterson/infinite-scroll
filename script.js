c5f79edadb2ec46627b68b508f9cae8333937b4b
sasi_p <sweetpea_th@hotmail.com>
2020-10-13 17:41:24 -0400

    Fetching data from Unsplash API.

---
+++ script.js
@@ -0,0 +1,18 @@
+// Unsplash API
+const count = 10;
+const apiKey ='ekiUoLqsfNXdBjs0FhQVu4NpwEAgK0BgQFbLaiOV_fA';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash Api 
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json ();
+        console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getPhotos();
